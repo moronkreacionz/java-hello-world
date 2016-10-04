d93f69f20e05b5be4e94ec61921e882275f1c3d6
moronkreacionz <moronkreacionz@gmail.com>
2016-10-04 17:46:27 +0530

    added 5 comments

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,5 +3,10 @@ public class JavaHelloWorld
 	public static void main(String [] args){
 		System.out.println("Java Hello World");
 		System.out.println("Hello again");
+		System.out.println("Hello again 01");
+		System.out.println("Hello again 02");
+		System.out.println("Hello again 03");
+		System.out.println("Hello again 04");
+		System.out.println("Hello again 05");
 	}
 }
